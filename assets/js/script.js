72a273d3d4d061ec8df920678b2cef76192c6974
mahmudfarooque <mahmud@jezweb.net>
2025-05-14 09:25:11 +0600

    #linkSection

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,24 @@ Loader Code Ends Here
 ---------------------------------------- */
 
  const currentYear = new Date().getFullYear();
-        document.getElementById('year').textContent = currentYear;
+document.getElementById('year').textContent = currentYear;
+
+
+
+// Function to scroll to an element with an offset (to avoid cutting off the title)
+function scrollToElementWithOffset(targetSelector, offset) {
+    const targetElement = document.querySelector(targetSelector);
+    if (targetElement) {
+        // Get the position of the target element
+        const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+
+        // Scroll to the target position with an offset
+        window.scrollTo({
+            top: targetPosition - offset,  // Adjust the scroll position to account for the offset
+            behavior: 'smooth'  // Smooth scroll effect
+        });
+    }
+}
+
+
+
